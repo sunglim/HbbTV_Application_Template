4f8e77368f369e88408edfa3206384c85e45189f
Lim Sungguk <limasdf@gmail.com>
2012-09-07 21:25:39 +0900

    Update Advanced/js/hbbtvlib.js
    added keyset

--- Advanced/js/hbbtvlib.js
+++ Advanced/js/hbbtvlib.js
@@ -2,4 +2,8 @@ function hbbtvlib_initialize(){
 	//should be called show() function, if not the application will not be shown;
 	var app = document.getElementById("oipfAppMan").getOwnerApplication(document);
 	app.show();
+	
+	//set keyset, only defined keys can be triggered on application.
+	// RED, GREEN, YELLOW, BLUE, NAVIGATION, VCR, NUMERIC
+	app.privateData.keyset.setValue(0x1+0x2+0x4+0x8+0x10+0x20+0x100);
 };
