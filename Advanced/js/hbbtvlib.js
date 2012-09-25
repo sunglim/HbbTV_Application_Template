727a058793b49c945102ccb1a7d599e43d1847a0
Lim Sungguk <limasdf@gmail.com>
2012-09-25 10:46:20 +0900

    Update Advanced/js/hbbtvlib.js

--- Advanced/js/hbbtvlib.js
+++ Advanced/js/hbbtvlib.js
@@ -1,3 +1,9 @@
+/*
+ * To use This library, You need to add the OIPF application manager objects to your HTML page,
+ * 
+ *	<object id="oipfAppMan" type="application/oipfApplicationManager"></object>
+ */
+ 
 function hbbtvlib_initialize(){
 	//should be called show() function, if not the application will not be shown;
 	var appManager = document.getElementById("oipfAppMan").getOwnerApplication(document);
