56542237b314ac56c07588af0dd467b32cdcfc91
Lim Sungguk <limasdf@gmail.com>
2012-09-25 10:41:32 +0900

    Update Advanced/js/hbbtvlib.js

--- Advanced/js/hbbtvlib.js
+++ Advanced/js/hbbtvlib.js
@@ -1,10 +1,10 @@
 function hbbtvlib_initialize(){
 	//should be called show() function, if not the application will not be shown;
-	var app = document.getElementById("oipfAppMan").getOwnerApplication(document);
-	app.show();
+	var appManager = document.getElementById("oipfAppMan").getOwnerApplication(document);
+	appManager.show();
 	
 	//set keyset, only defined keys can be triggered on application.
 	// RED, GREEN, YELLOW, BLUE, NAVIGATION, VCR, NUMERIC
 	//app.private.keyset.setValue(0x1+0x2+0x4+0x8+0x10+0x20+0x100);  // official
-	app.privateData.keyset.setValue(0x1+0x2+0x4+0x8+0x10+0x20+0x100);
+	appManager.privateData.keyset.setValue(0x1+0x2+0x4+0x8+0x10+0x20+0x100);
 };
